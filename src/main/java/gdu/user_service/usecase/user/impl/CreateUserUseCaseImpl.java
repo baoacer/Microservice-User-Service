286c4839043401f01732128dcebf5ddd9050b9df
baoacer <nguyenquocbaotu@gmail.com>
2025-03-30 21:22:26 +0700

    update role

--- src/main/java/gdu/user_service/usecase/user/impl/CreateUserUseCaseImpl.java
+++ src/main/java/gdu/user_service/usecase/user/impl/CreateUserUseCaseImpl.java
@@ -2,6 +2,7 @@ package gdu.user_service.usecase.user.impl;
 
 import gdu.user_service.entity.RoleEntity;
 import gdu.user_service.entity.UserEntity;
+import gdu.user_service.exception.ConflictException;
 import gdu.user_service.exception.NotFoundException;
 import gdu.user_service.repository.RoleRepository;
 import gdu.user_service.repository.UserRepository;
@@ -21,14 +22,20 @@ public class CreateUserUseCaseImpl implements CreateUserUseCase {
 
     private final UserRepository userRepository;
     private final RoleRepository roleRepository;
-    private static final String role = "ROLE_USER";
+    private final String roleDefault = "ROLE_USER";
 
     @Override
     public UserDto execute(CreateUserRequest request) {
+        System.out.println("==================" + request.getRoleName());
         UserEntity foundUser = this.userRepository.findByEmail(request.getEmail());
-        if(foundUser != null) throw new NotFoundException("User not found");
+        if(foundUser != null) throw new ConflictException("Email already exists");
 
-        RoleEntity userRole = this.roleRepository.findByName(role);
+        RoleEntity userRole;
+        if(request.getRoleName() != null){
+            userRole = this.roleRepository.findByName(request.getRoleName());
+        }else{
+            userRole = this.roleRepository.findByName(roleDefault);
+        }
 
         PasswordEncoder passwordEncoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
 
